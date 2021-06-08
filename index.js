ca54d0b445469fc8fa66d703d97a62125e027011
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-06-08 22:02:13 +0200

    rebuild

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 require('dotenv').config()
+const express = require('express')
 const {Telegraf} = require('telegraf')
 const {actionHandler} = require('./functions')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.replyWithMarkdown(`Welcome to *${ctx.botInfo.first_name}*
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+	console.log(`Server has been started on port ${PORT}...`)
+	bot.start((ctx) => ctx.replyWithMarkdown(`Welcome to *${ctx.botInfo.first_name}*
 Введите пару валют что бы получить текущий курс или напишите /go что бы увидеть некоторые пары.
 _Регистр написания не имеет значения.
 Например:_ *WEC USD*, *ra usd*, *doge usd*
 
 Ваши предложения по улучшению и замечания отправляйте *@Cocaine_GG*`))
 
-bot.command('go', ctx => {
-	ctx.telegram.sendMessage(ctx.chat.id, 'Выберете пару', {
-		reply_markup: {
-			resize_keyboard: true,
-			// one_time_keyboard: true,
-			keyboard: [
-				[
-					{ text: 'WEC USD', callback_data: 'wec usd' },
-					{ text: 'RA USD', callback_data: 'ra usd' },
-					{ text: 'WEC RA', callback_data: 'wec ra' }
-				],
-			]
-		}
+	bot.command('go', ctx => {
+		ctx.telegram.sendMessage(ctx.chat.id, 'Выберете пару', {
+			reply_markup: {
+				resize_keyboard: true, // one_time_keyboard: true,
+				keyboard: [[{text: 'WEC USD', callback_data: 'wec usd'}, {
+					text: 'RA USD', callback_data: 'ra usd'
+				}, {text: 'WEC RA', callback_data: 'wec ra'}],]
+			}
+		})
 	})
-})
 
-bot.hears(/\w/, actionHandler)
-bot.action('wec usd', actionHandler)
-bot.action('wec ra', actionHandler)
-bot.action('ra usd', actionHandler)
-bot.launch()
+	bot.hears(/\w/, actionHandler)
+	bot.action('wec usd', actionHandler)
+	bot.action('wec ra', actionHandler)
+	bot.action('ra usd', actionHandler)
+	bot.launch()
+})
